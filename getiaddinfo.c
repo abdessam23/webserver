41ee2257d104633dc396ae631349a0850214edda
userhm <dealkalr@gmail.com>
2026-02-07 12:14:16 +0100

    ok

--- getiaddinfo.c
+++ getiaddinfo.c
@@ -1,92 +1,58 @@
-#include <netdb.h>
+
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
 #include <sys/types.h>
-#include <unistd.h>
-#define BUF_SIZE 50
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
-       int
-       main(int argc, char *argv[])
-       {
-           int              sfd, s;
-           char             buf[BUF_SIZE];
-           size_t           len;
-           ssize_t          nread;
-           struct addrinfo  hints;      
-           struct addrinfo  *result, *rp;
+int main(int argc, char *argv[])
+{
+    struct addrinfo *hints, *res, *p;
+    int status;
+    char ipstr[INET6_ADDRSTRLEN];
 
-           if (argc < 3) {
-               fprintf(stderr, "Usage: %s host port msg...\n", argv[0]);
-               exit(EXIT_FAILURE);
-           }
+    if (argc != 2) {
+        fprintf(stderr,"usage: showip hostname\n");
+        return 1;
+    }
 
-           /* Obtain address(es) matching host/port. */
+    memset(&hints, 0, sizeof hints);
+    hints->ai_family = AF_UNSPEC;  // Either IPv4 or IPv6 
+    hints->ai_socktype = SOCK_STREAM;     
 
-           memset(&hints, 0, sizeof(hints));
-           hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
-           hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
-           hints.ai_flags = 0;
-           hints.ai_protocol = 0;          /* Any protocol */
+    if ((status = getaddrinfo(argv[1], NULL, hints, &res)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+        return 2;
+    }
 
-           s = getaddrinfo(argv[1], argv[2], &hints, &result);
-           if (s != 0) {
-               fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
-               exit(EXIT_FAILURE);
-           }
+    printf("IP addresses for %s:\n\n", argv[1]);
 
-           /* getaddrinfo() returns a list of address structures.
-              Try each address until we successfully connect(2).
-              If socket(2) (or connect(2)) fails, we (close the socket
-              and) try the next address. */
-                       for (rp = result; rp != NULL; rp = rp->ai_next) {
-               sfd = socket(rp->ai_family, rp->ai_socktype, 
-                            rp->ai_protocol);
-               if (sfd == -1)
-                   continue;
+    for(p = res;p != NULL; p = p->ai_next) {
+        void *addr;
+        char *ipver;
+        struct sockaddr_in *ipv4;
+        struct sockaddr_in6 *ipv6;
 
-               if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1)
-                   break;                  /* Success */
+        // get the pointer to the address itself,
+        // different fields in IPv4 and IPv6:
+        if (p->ai_family == AF_INET) { // IPv4
+            ipv4 = (struct sockaddr_in *)p->ai_addr;
+            addr = &(ipv4->sin_addr);
+            ipver = "IPv4";
+        } else { // IPv6
+            ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+            addr = &(ipv6->sin6_addr);
+            ipver = "IPv6";
+        }
 
-               close(sfd);
-           }
+        // convert the IP to a string and print it:
+        inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+        printf("  %s: %s\n", ipver, ipstr);
+    }
 
-           freeaddrinfo(result);           /* No longer needed */
+    freeaddrinfo(res); // free the linked list
 
-           if (rp == NULL) {               /* No address succeeded */
-               fprintf(stderr, "Could not connect\n");
-               exit(EXIT_FAILURE);
-           }
-
-           /* Send remaining command-line arguments as separate
-              datagrams, and read responses from server. */
-
-           for (size_t j = 3; j < argc; j++) {
-               len = strlen(argv[j]) + 1;
-                       /* +1 for terminating null byte */
-
-               if (len > BUF_SIZE) {
-                   fprintf(stderr,
-                           "Ignoring long message in argument %zu\n", j);
-                   continue;
-               }
-
-               if (write(sfd, argv[j], len) != len) {
-                   fprintf(stderr, "partial/failed write\n");
-                      exit(EXIT_FAILURE);
-               }
-
-               nread = read(sfd, buf, BUF_SIZE);
-               if (nread == -1) {
-                   perror("read");
-                   exit(EXIT_FAILURE);
-               }
-
-               printf("Received %zd bytes: %s\n", nread, buf);
-           }
-
-           exit(EXIT_SUCCESS);
-       }
-
-
+    return 0;
+}
