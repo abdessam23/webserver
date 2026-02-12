18ddd1be3a5e71a34d4beab6303e8aa871024970
userhm <dealkalr@gmail.com>
2026-02-12 12:49:19 +0100

    test

--- test.c
+++ test.c
@@ -6,8 +6,8 @@
 
 int main(int ac,char** arg)
 {
-    char buf[10];
-    int n = read(0,buf,10);
-    printf("%s", buf);
-    return 0;
+    // char buf[10];
+    // int n = read(0,buf,10);
+    // printf("%s", buf);
+    // return 0;
 }
