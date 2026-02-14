78736ceaa81dd8da8f4dbc965d67316b25801746
userhm <dealkalr@gmail.com>
2026-02-14 10:38:22 +0100

    test

---
+++ serv.c
@@ -0,0 +1,55 @@
+#include <netdb.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main()
+{
+    struct  addrinfo hints,*res;
+    char buf[100];
+    hints.ai_family= AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    if(getaddrinfo("localhost","8080", &hints,&res) != 0)
+    {
+        printf("hello");
+        return 1;
+    }
+    struct addrinfo *rp;
+    int sfd;
+    for(rp = res; rp != NULL;rp = rp->ai_next) 
+    {
+
+        sfd = socket(rp->ai_family,rp->ai_socktype,rp->ai_protocol);
+        if (sfd == -1)
+            continue;
+        if (bind(sfd,(struct sockaddr*)rp->ai_addr,rp->ai_addrlen) == 0)
+        {
+            printf("fd : %d",sfd);
+            break;
+        }
+        close(sfd);
+    }
+    if (rp == NULL)
+    {
+      printf("yes it is null\n");
+      return 1;  
+    }
+    while (1)
+    {
+        int n = read(sfd,buf,10);
+        if (n == 0)
+            continue;
+        if(n == -1)
+        {
+            printf("read failed.");
+            return 1;
+        }
+        else 
+            printf("buf : %s",buf);
+    }
+    
+
+    return 0;
+    
+}
