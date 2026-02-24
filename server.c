78762160eca64b7d2809d86a973740e6144903d2
userhm <dealkalr@gmail.com>
2026-02-24 12:44:47 +0000

    ok

--- server.c
+++ server.c
@@ -7,14 +7,14 @@
 
 int main() {
 
-    int server_fd = socket(AF_INET,SOCK_STREAM, 0); 
-    if (server_fd == -1) { perror("socket failed"); exit(1); }
+    // int server_fd = socket(AF_INET,SOCK_STREAM, 0); 
+    // if (server_fd == -1) { perror("socket failed"); exit(1); }
 
    
-    struct sockaddr_in address;
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY; // Listen on 0.0.0.0 (any interface)
-    address.sin_port = htons(8080);       // Port 8080 (converted to network byte order)
+    // struct sockaddr_in address;
+    // address.sin_family = AF_INET;
+    // address.sin_addr.s_addr = INADDR_ANY; // Listen on 0.0.0.0 (any interface)
+    // address.sin_port = htons(8080);       // Port 8080 (converted to network byte order)
 
     // 3. Bind the socket to the address
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
