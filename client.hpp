df3bc0bc7633db468a255e4c4780dab5c110b07a
userhm <dealkalr@gmail.com>
2026-03-06 11:57:28 +0000

    ok

---
+++ client.hpp
@@ -0,0 +1,22 @@
+#pragma once 
+#include <iostream>
+#include <map>
+
+class Client
+{
+private:
+    std::string write_buff;
+    std::string read_buff;
+public:
+    Client();
+    
+    ~Client();
+};
+
+Client::Client()
+{
+}
+
+Client::~Client()
+{
+}
---
+++ master.hpp
@@ -0,0 +1,84 @@
+#pragma once 
+#include <netdb.h>
+#include <unistd.h>
+#include <sys/epoll.h>
+#include <exception>
+#include "client.hpp"
+#define max_event 10
+
+class Master
+{
+private:
+    std::map<int,Client> m; 
+public:
+    Master();
+    void run();
+    ~Master();
+};
+
+void Master::run()
+{
+    struct sockaddr_in hint;
+    socklen_t len;
+    
+    int socket_fd = socket(AF_INET,SOCK_STREAM,0);
+    if (socket_fd == -1)
+    {
+        throw  std::runtime_error("Socket failed .");
+    }
+    if (bind(socket_fd,(struct sockaddr*)&hint,len) != 0)
+    {
+        throw  std::runtime_error("bind failed .");
+    }
+    if (listen(socket_fd,10) != 0)
+    {
+        throw  std::runtime_error("bind failed .");
+    }
+    int epoll_fd = epoll_create(0);
+    struct epoll_event event;
+
+    if (epoll_ctl(epoll_fd,EPOLL_CTL_ADD,socket_fd,&event) != 0)
+    {
+        perror("epoll_ctl failed .");
+        return ;
+    }
+    struct  epoll_event  arr_event[max_event];
+    
+    while (1)
+    {
+        struct sockaddr_in client_add;
+        socklen_t len_add = sizeof(client_add);
+        
+        int active_ev = epoll_wait(epoll_fd,arr_event,max_event,-1);
+        if(active_ev == -1)
+            break; // what should do here ??
+        for(int i = 0; i < active_ev;i++)
+        {
+            if (arr_event[i].data.fd == socket_fd)
+            {
+                int newfd = accept(socket_fd,(struct sockaddr*)&client_add, &len_add);
+                if (epoll_ctl(epoll_fd,EPOLL_CTL_ADD,socket_fd,&event) != 0)
+                {
+                    perror("epoll_ctl failed .");
+                    return ;
+                }
+            }
+            else if (event.events && EPOLLIN ) //??
+            {
+                //read data; .........
+            }
+            else if (event.events && EPOLLOUT){
+                //write data;..........
+            }
+        }
+    }
+    
+}
+
+Master::Master()
+{
+}
+
+Master::~Master()
+{
+}
